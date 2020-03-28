24ece6feabc45364e953f038ac46b084da98af95
Alan <alanmagelssen@gmail.com>
2020-03-28 12:25:04 -0700

    scene change happenin'

--- Assets/Utilities/PocketDroidConstants.cs
+++ Assets/Utilities/PocketDroidConstants.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class PocketDroidConstants : MonoBehaviour {
 
-	public static string SCENE_WORLD = "World";
-	public static string SCENE_CAPTURE = "Capture"; 
+	public static string SCENE_WORLD = "World2";
+	public static string SCENE_CAPTURE = "Capture2"; 
 
 	public static string TAG_DROID = "Droid";
 	public static string TAG_OVERRIDE_ORB = "Override Orb";
